ea1e776086d910507ecf04fa9301047c6e5a6e6d
Istvan <up2018426@myport.ac.uk>
2020-12-27 00:51:18 +0000

    URP + Shaders

---
+++ DungeonCrawler/Assets/WeaponShaderScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShaderScript : MonoBehaviour
+{
+    public float height;
+    public float TopRange;
+    public float BottomRange;
+    public Material thisMat;
+
+    public bool collaped;
+
+    public float speeeeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //thisMat.SetFloat("Vector1_F9AEF008", height);
+        if (Input.GetKeyDown("e"))
+        {
+            collaped = false;
+        }
+
+        if (Input.GetKeyDown("q"))
+        {
+            collaped = true;
+        }
+
+        if(collaped == true)
+        {
+            height = Mathf.Lerp(thisMat.GetFloat("Vector1_F9AEF008"), BottomRange, speeeeed * Time.deltaTime);
+        }
+
+        if(collaped == false)
+        {
+            height = Mathf.Lerp(thisMat.GetFloat("Vector1_F9AEF008"), TopRange, speeeeed * Time.deltaTime);
+        }
+
+        thisMat.SetFloat("Vector1_F9AEF008", height);
+        /*if (Input.GetKey("q"))
+        {
+            height += (8f * Time.deltaTime);
+        }
+        if (Input.GetKey("e"))
+        {
+            height -= (8f * Time.deltaTime);
+        }*/
+    }
+}
