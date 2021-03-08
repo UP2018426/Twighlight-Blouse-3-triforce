3ea0687cca5f005d6a0b6111dd22887994dd442d
Istvan <up2018426@myport.ac.uk>
2021-03-08 19:12:39 +0000

    Tech Demo + Cursor update

--- DungeonCrawler/Assets/Scripts/PlayerController.cs
+++ DungeonCrawler/Assets/Scripts/PlayerController.cs
@@ -129,8 +129,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
+        //Istvan wrote this
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        //my bad bro
 
 
         if (Input.GetButtonDown("Cancel"))
