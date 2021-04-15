b6668a14f7f53a4c791ad04fc8e920949fbe0d74
RadJohnson <73299935+RadJohnson@users.noreply.github.com>
2021-04-15 22:06:04 +0100

    Lower Filesize

--- DungeonCrawler/Assets/Scripts/IstvanRoomGenDelete.cs
+++ DungeonCrawler/Assets/Scripts/IstvanRoomGenDelete.cs
@@ -304,275 +304,262 @@ public class IstvanRoomGenDelete : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (!CheckGensize.delete)
-        //{
-        //    Delete();
-        //}
-        //if (!CheckGensize.check && CheckGensize.logDungeon)
-        //{
-        //    RoomChecking();
-        //}
-        //if (!CheckGensize.spawn && CheckGensize.log1Rooms)
-        //{
-        //    Spawn();
-        //}
-
         if(boom)
         {
             Destroy(gameObject);
         }
 
 
-        if (Input.GetKeyDown("k"))
-        {
-            randomNum = Random.Range(1, 9);
-        }
+    //    if (Input.GetKeyDown("k"))
+    //    {
+    //        randomNum = Random.Range(1, 9);
+    //    }
 
-        if (randomNum == 5)
-        {
-            Debug.Log("destroyed");
-            Destroy(this.gameObject);
-        }
+    //    if (randomNum == 5)
+    //    {
+    //        Debug.Log("destroyed");
+    //        Destroy(this.gameObject);
+    //    }
 
-        if (Input.GetKeyDown("j"))
-        {
-            RaycastHit hit;
+    //    if (Input.GetKeyDown("j"))
+    //    {
+    //        RaycastHit hit;
 
-            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), out hit, dist))
-            {
-                fwd = true;
-            }
+    //        if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), out hit, dist))
+    //        {
+    //            fwd = true;
+    //        }
 
-            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.right), out hit, dist))
-            {
-                rgt = true;
-            }
+    //        if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.right), out hit, dist))
+    //        {
+    //            rgt = true;
+    //        }
 
-            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.back), out hit, dist))
-            {
-                bck = true;
-            }
+    //        if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.back), out hit, dist))
+    //        {
+    //            bck = true;
+    //        }
 
-            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.left), out hit, dist))
-            {
-                lft = true;
-            }
+    //        if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.left), out hit, dist))
+    //        {
+    //            lft = true;
+    //        }
 
-            for (int i = 0; i < CheckGensize.grid.Count; i++)
-            {
-                //for checkign how many conected
-                if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), out hit, dist) && isConected == true)
-                {
-                    hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
-                    Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
-                }
+    //        for (int i = 0; i < CheckGensize.grid.Count; i++)
+    //        {
+    //            //for checkign how many conected
+    //            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), out hit, dist) && isConected == true)
+    //            {
+    //                hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
+    //                Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
+    //            }
 
-                if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.right), out hit, dist) && isConected == true)
-                {
-                    hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
-                    Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
-                }
+    //            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.right), out hit, dist) && isConected == true)
+    //            {
+    //                hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
+    //                Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
+    //            }
 
-                if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.back), out hit, dist) && isConected == true)
-                {
-                    hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
-                    Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
-                }
+    //            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.back), out hit, dist) && isConected == true)
+    //            {
+    //                hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
+    //                Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
+    //            }
 
-                if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.left), out hit, dist) && isConected == true)
-                {
-                    hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
-                    Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
-                }
-            }
+    //            if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.left), out hit, dist) && isConected == true)
+    //            {
+    //                hit.transform.gameObject.GetComponent<IstvanRoomGenDelete>().isConected = true;
+    //                Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.up) * 50, Color.magenta, 100f);
+    //            }
+    //        }
 
-            corners = 0;
+    //        corners = 0;
 
-            if (fwd == true)
-            {
-                corners++;
-            }
-            if (rgt == true)
-            {
-                corners++;
-            }
-            if (bck == true)
-            {
-                corners++;
-            }
-            if (lft == true)
-            {
-                corners++;
-            }
-
-
-            /*if(lft && rgt && fwd && bck == false)
-            {
-                Destroy(gameObject);
-            }*/
-            /*Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.red);
-            Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.right) * 10, Color.green);
-            Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.back) * 10, Color.blue);
-            Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.left) * 10, Color.white);*/
-        }
-
-        if (Input.GetKeyDown("h"))
-        {
+    //        if (fwd == true)
+    //        {
+    //            corners++;
+    //        }
+    //        if (rgt == true)
+    //        {
+    //            corners++;
+    //        }
+    //        if (bck == true)
+    //        {
+    //            corners++;
+    //        }
+    //        if (lft == true)
+    //        {
+    //            corners++;
+    //        }
 
 
-            if (corners == 4) //just spawn a 4way
-            {
-                Instantiate(r5, this.transform.position, Quaternion.identity);
-            }
+    //        /*if(lft && rgt && fwd && bck == false)
+    //        {
+    //            Destroy(gameObject);
+    //        }*/
+    //        /*Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.red);
+    //        Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.right) * 10, Color.green);
+    //        Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.back) * 10, Color.blue);
+    //        Debug.DrawRay(this.transform.position, transform.TransformDirection(Vector3.left) * 10, Color.white);*/
+    //    }
 
-            if (corners == 3) // just spawn a 3way + rotate
-            {
-                if (fwd == false)
-                {
-                    Instantiate(r4, this.transform.position, Quaternion.Euler(0, 90, 0));
-                }
-                if (rgt == false)
-                {
-                    Instantiate(r4, this.transform.position, Quaternion.Euler(0, 180, 0));
-                }
-                if (bck == false)
-                {
-                    Instantiate(r4, this.transform.position, Quaternion.Euler(0, 270, 0));
-                }
-                if (lft == false)
-                {
-                    Instantiate(r4, this.transform.position, Quaternion.Euler(0, 0, 0));
-                }
-            }
+    //    if (Input.GetKeyDown("h"))
+    //    {
 
-            if (corners == 2)//find L or straight + rotate
-            {
-                if (fwd && bck == true || lft && rgt == true)
-                {
-                    Debug.Log("straight");
-                    if (fwd == true)
-                    {
-                        Instantiate(r3, this.transform.position, Quaternion.Euler(0, 0, 0));
-                    }
 
-                    if (rgt == true)
-                    {
-                        Instantiate(r3, this.transform.position, Quaternion.Euler(0, 90, 0));
-                    }
+    //        if (corners == 4) //just spawn a 4way
+    //        {
+    //            Instantiate(r5, this.transform.position, Quaternion.identity);
+    //        }
 
-                }
+    //        if (corners == 3) // just spawn a 3way + rotate
+    //        {
+    //            if (fwd == false)
+    //            {
+    //                Instantiate(r4, this.transform.position, Quaternion.Euler(0, 90, 0));
+    //            }
+    //            if (rgt == false)
+    //            {
+    //                Instantiate(r4, this.transform.position, Quaternion.Euler(0, 180, 0));
+    //            }
+    //            if (bck == false)
+    //            {
+    //                Instantiate(r4, this.transform.position, Quaternion.Euler(0, 270, 0));
+    //            }
+    //            if (lft == false)
+    //            {
+    //                Instantiate(r4, this.transform.position, Quaternion.Euler(0, 0, 0));
+    //            }
+    //        }
 
-                else
-                {
-                    Debug.Log("L");
-                    if (fwd == true)
-                    {
-                        if (rgt == true)
-                        {
-                            Instantiate(r2, this.transform.position, Quaternion.Euler(0, 0, 0));
-                        }
+    //        if (corners == 2)//find L or straight + rotate
+    //        {
+    //            if (fwd && bck == true || lft && rgt == true)
+    //            {
+    //                Debug.Log("straight");
+    //                if (fwd == true)
+    //                {
+    //                    Instantiate(r3, this.transform.position, Quaternion.Euler(0, 0, 0));
+    //                }
 
-                        else
-                        {
-                            Instantiate(r2, this.transform.position, Quaternion.Euler(0, 270, 0));
-                        }
-                    }
+    //                if (rgt == true)
+    //                {
+    //                    Instantiate(r3, this.transform.position, Quaternion.Euler(0, 90, 0));
+    //                }
 
-                    if (bck == true)
-                    {
-                        if (rgt == true)
-                        {
-                            Instantiate(r2, this.transform.position, Quaternion.Euler(0, 90, 0));
-                        }
+    //            }
 
-                        else
-                        {
-                            Instantiate(r2, this.transform.position, Quaternion.Euler(0, 180, 0));
-                        }
-                    }
-                }
-            }
+    //            else
+    //            {
+    //                Debug.Log("L");
+    //                if (fwd == true)
+    //                {
+    //                    if (rgt == true)
+    //                    {
+    //                        Instantiate(r2, this.transform.position, Quaternion.Euler(0, 0, 0));
+    //                    }
 
-            if (corners == 1 & !isBeegBoss & !start)//spawn a 1 + rotate
-            {
-                if (fwd == true)
-                {
-                    Instantiate(r1, this.transform.position, Quaternion.Euler(0, 0, 0));
-                }
+    //                    else
+    //                    {
+    //                        Instantiate(r2, this.transform.position, Quaternion.Euler(0, 270, 0));
+    //                    }
+    //                }
 
-                if (rgt == true)
-                {
-                    Instantiate(r1, this.transform.position, Quaternion.Euler(0, 90, 0));
-                }
+    //                if (bck == true)
+    //                {
+    //                    if (rgt == true)
+    //                    {
+    //                        Instantiate(r2, this.transform.position, Quaternion.Euler(0, 90, 0));
+    //                    }
 
-                if (bck == true)
-                {
-                    Instantiate(r1, this.transform.position, Quaternion.Euler(0, 180, 0));
-                }
+    //                    else
+    //                    {
+    //                        Instantiate(r2, this.transform.position, Quaternion.Euler(0, 180, 0));
+    //                    }
+    //                }
+    //            }
+    //        }
 
-                if (lft == true)
-                {
-                    Instantiate(r1, this.transform.position, Quaternion.Euler(0, 270, 0));
-                }
-            }
+    //        if (corners == 1 & !isBeegBoss & !start)//spawn a 1 + rotate
+    //        {
+    //            if (fwd == true)
+    //            {
+    //                Instantiate(r1, this.transform.position, Quaternion.Euler(0, 0, 0));
+    //            }
 
-            if (corners == 1 && isBeegBoss & !start)//spawn end + rotate
-            {
-                if (fwd == true)
-                {
-                    Instantiate(r6, this.transform.position, Quaternion.Euler(0, 0, 0));
-                }
+    //            if (rgt == true)
+    //            {
+    //                Instantiate(r1, this.transform.position, Quaternion.Euler(0, 90, 0));
+    //            }
 
-                if (rgt == true)
-                {
-                    Instantiate(r6, this.transform.position, Quaternion.Euler(0, 90, 0));
-                }
+    //            if (bck == true)
+    //            {
+    //                Instantiate(r1, this.transform.position, Quaternion.Euler(0, 180, 0));
+    //            }
 
-                if (bck == true)
-                {
-                    Instantiate(r6, this.transform.position, Quaternion.Euler(0, 180, 0));
-                }
+    //            if (lft == true)
+    //            {
+    //                Instantiate(r1, this.transform.position, Quaternion.Euler(0, 270, 0));
+    //            }
+    //        }
 
-                if (lft == true)
-                {
-                    Instantiate(r6, this.transform.position, Quaternion.Euler(0, 270, 0));
-                }
-            }
+    //        if (corners == 1 && isBeegBoss & !start)//spawn end + rotate
+    //        {
+    //            if (fwd == true)
+    //            {
+    //                Instantiate(r6, this.transform.position, Quaternion.Euler(0, 0, 0));
+    //            }
 
-            if (corners == 1 & !isBeegBoss && start)//spawn start + rotate
-            {
-                if (fwd == true)
-                {
-                    Instantiate(r7, this.transform.position, Quaternion.Euler(0, 0, 0));
-                }
+    //            if (rgt == true)
+    //            {
+    //                Instantiate(r6, this.transform.position, Quaternion.Euler(0, 90, 0));
+    //            }
 
-                if (rgt == true)
-                {
-                    Instantiate(r7, this.transform.position, Quaternion.Euler(0, 90, 0));
-                }
+    //            if (bck == true)
+    //            {
+    //                Instantiate(r6, this.transform.position, Quaternion.Euler(0, 180, 0));
+    //            }
 
-                if (bck == true)
-                {
-                    Instantiate(r7, this.transform.position, Quaternion.Euler(0, 180, 0));
-                }
+    //            if (lft == true)
+    //            {
+    //                Instantiate(r6, this.transform.position, Quaternion.Euler(0, 270, 0));
+    //            }
+    //        }
 
-                if (lft == true)
-                {
-                    Instantiate(r7, this.transform.position, Quaternion.Euler(0, 270, 0));
-                }
-            }
+    //        if (corners == 1 & !isBeegBoss && start)//spawn start + rotate
+    //        {
+    //            if (fwd == true)
+    //            {
+    //                Instantiate(r7, this.transform.position, Quaternion.Euler(0, 0, 0));
+    //            }
 
-            //BELOW THIS ARE THE ERROR STATES
+    //            if (rgt == true)
+    //            {
+    //                Instantiate(r7, this.transform.position, Quaternion.Euler(0, 90, 0));
+    //            }
 
-            if (corners > 4)
-            {
-                Debug.LogWarning(this.name + " is reading more than 4 doors!");
-            }
+    //            if (bck == true)
+    //            {
+    //                Instantiate(r7, this.transform.position, Quaternion.Euler(0, 180, 0));
+    //            }
 
-            if (corners < 1)
-            {
-                Debug.LogWarning(this.name + " is reading less than 1 door!");
-            }
-        }
+    //            if (lft == true)
+    //            {
+    //                Instantiate(r7, this.transform.position, Quaternion.Euler(0, 270, 0));
+    //            }
+    //        }
+
+    //        //BELOW THIS ARE THE ERROR STATES
+
+    //        if (corners > 4)
+    //        {
+    //            Debug.LogWarning(this.name + " is reading more than 4 doors!");
+    //        }
+
+    //        if (corners < 1)
+    //        {
+    //            Debug.LogWarning(this.name + " is reading less than 1 door!");
+    //        }
+    //    }
     }
 }
