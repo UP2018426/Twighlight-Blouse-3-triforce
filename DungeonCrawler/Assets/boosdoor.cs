65415a1587ac8ed5cb02dbe1f89c64b0ebc9191d
jcursley <76122203+jcursley@users.noreply.github.com>
2021-05-04 19:10:32 +0100

    Revert "Merge branch 'Test' of https://github.com/UP2018426/Twighlight-Blouse-3-triforce into Test"
    This reverts commit 69673be6638b590c5cf213ef968c319330eb0fed, reversing
    changes made to 4e27c1d3862292dd82af49c1327bdcc2fc723fea.

---
+++ DungeonCrawler/Assets/boosdoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boosdoor : MonoBehaviour
+{
+    GameObject keyEnemy;
+
+    bool done;
+
+    void Update()
+    {
+        if(keyEnemy == null &! done)
+        {
+            this.transform.position += Vector3.up * 5;
+
+            done = true;
+        }
+    }
+}
